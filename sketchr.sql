5a2f3a1f1d1c8da35e72d428f184363ea0e260d1
lamottin <quentin.lamotte@outlook.com>
2014-02-05 21:15:37 +0100

    SQL file for website database

---
+++ sketchr.sql
@@ -0,0 +1,171 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 05 Février 2014 à 19:48
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `sketchr`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `abuse_noted`
+--
+
+CREATE TABLE IF NOT EXISTS `abuse_noted` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `processed` tinyint(1) NOT NULL,
+  `comment` int(11) NOT NULL,
+  `member` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `post_date` date NOT NULL,
+  `sketch` int(11) NOT NULL,
+  `member` int(11) NOT NULL,
+  `precomment` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dead_link`
+--
+
+CREATE TABLE IF NOT EXISTS `dead_link` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `processed` tinyint(1) NOT NULL,
+  `sketch` int(11) NOT NULL,
+  `member` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fan_humorist`
+--
+
+CREATE TABLE IF NOT EXISTS `fan_humorist` (
+  `member` int(11) NOT NULL,
+  `humorist` int(11) NOT NULL,
+  PRIMARY KEY (`member`,`humorist`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fan_sketch_type`
+--
+
+CREATE TABLE IF NOT EXISTS `fan_sketch_type` (
+  `member` int(11) NOT NULL,
+  `sketch_type` int(11) NOT NULL,
+  PRIMARY KEY (`member`,`sketch_type`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `humorist`
+--
+
+CREATE TABLE IF NOT EXISTS `humorist` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `birthday` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `member`
+--
+
+CREATE TABLE IF NOT EXISTS `member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `country` varchar(30) NOT NULL,
+  `birthday` date NOT NULL,
+  `member_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sketch`
+--
+
+CREATE TABLE IF NOT EXISTS `sketch` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `video_link` varchar(300) NOT NULL,
+  `release_date` date NOT NULL,
+  `synopsis` text NOT NULL,
+  `sketch_type` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sketch_type`
+--
+
+CREATE TABLE IF NOT EXISTS `sketch_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `start_date` date NOT NULL,
+  `synopsis` text NOT NULL,
+  `category` int(11) NOT NULL,
+  `humorist` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
