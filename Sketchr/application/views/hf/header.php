88135294afe8dfd121e322c9df02a08b78af9a8c
lamottin <quentin.lamotte@outlook.com>
2014-02-09 20:49:03 +0100

    Modification SQL database file & home web page

--- Sketchr/application/views/hf/header.php
+++ Sketchr/application/views/hf/header.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Sketchr</title>
+		<link type="image/ico" rel="shortcut icon" href="assets/logo/logo.ico">
 		<link rel="stylesheet" href="assets/foundation/css/foundation.css" />
 		<script type="text/javascript" src="assets/foundation/js/vendor/modernizr.js"></script>
 	</head>
