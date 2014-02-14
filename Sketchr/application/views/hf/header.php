4e45900736d68bdc47e3ee3dde380a661250dd0a
ekanseht <thesnake973@gmail.com>
2014-02-15 00:13:43 +0100

    header - navbar
    Navbar not styled

--- Sketchr/application/views/hf/header.php
+++ Sketchr/application/views/hf/header.php
@@ -6,6 +6,46 @@
 		<title>Sketchr</title>
 		<link type="image/ico" rel="shortcut icon" href="assets/logo/logo.ico">
 		<link rel="stylesheet" href="assets/foundation/css/foundation.css" />
+		<link rel="stylesheet" href="assets/foundation/css/app.css" />
+		<script type="text/javascript" src="assets/foundation/js/vendor/jquery.js"></script>
 		<script type="text/javascript" src="assets/foundation/js/vendor/modernizr.js"></script>
+		<script type="text/javascript" src="assets/foundation/js/foundation.min.js"></script>
+		<script type="text/javascript" src="assets/foundation/js/foundation/foundation.topbar.js"></script>
 	</head>
 	<body>
+
+	<nav class="top-bar" data-topbar>
+		<ul class="title-area">
+			<li class="name large-8 small-9 columns">
+				<h1><a href="#">Sketchr</a></h1>
+			</li>
+			<li class="toggle-topbar menu-icon">
+				<a href="#">Menu</a>
+			</li>
+		</ul>
+
+		<section class="top-bar-section">
+			<!-- Left Nav Section -->
+			<ul class="left">
+				<form action="">
+					<li class="has-form">
+						<div class="row collapse">
+							<div class="large-10 small-9 columns">
+								<input type="text" placeholder="Find Stuff">
+							</div>
+							<div class="large-2 small-3 columns">
+								<a href="#" class="alert button expand">Search</a>
+							</div>
+						</div>
+					</li>
+				</form>
+			</ul>
+			
+			<!-- Right Nav Section -->
+			<ul class="right">
+				<li class="has-form">
+					<a href="http://foundation.zurb.com/docs" class="button radius">Sign in</a>
+				</li>
+			</ul>
+		</section>
+	</nav>
