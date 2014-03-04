044f15aaf9b7b9b58f4a61275245ea15435bde03
ekanseht <thesnake973@gmail.com>
2014-03-04 18:46:57 +0100

    Dynamic Base_Url
    Added a dynamic way to redirect to the site localy for each contributor
    within config.php

--- Sketchr/application/config/config.php
+++ Sketchr/application/config/config.php
@@ -14,7 +14,10 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1:8088/projects/GH/sketchr/Sketchr/';
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$config['base_url']    = "$root";
 
 /*
 |--------------------------------------------------------------------------
